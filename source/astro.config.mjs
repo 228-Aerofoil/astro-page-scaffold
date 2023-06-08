fe3c525bea937889b5e8b27c9cea57727100d99f
Michael Kassabov <35264484+Mkassabov@users.noreply.github.com>
2023-06-07 21:22:29 -0400

    Update astro.config.mjs

--- source/astro.config.mjs
+++ source/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	outDir: "dist/output",
-	adapter: cloudflare(),
+  output: "server",
+  outDir: "dist",
+  adapter: cloudflare(),
 });
