dc5001abfc30f6210b4f4124d36ad99c31f36e9c
Michael Kassabov <35264484+Mkassabov@users.noreply.github.com>
2023-07-03 20:11:32 -0400

    env support

--- source/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import cloudflare from "@astrojs/cloudflare";
-import { defineConfig } from "astro/config";
-
-// https://astro.build/config
-export default defineConfig({
-  output: "server",
-  outDir: "dist",
-  adapter: cloudflare(),
-});
--- source/src/env.d.ts
+++ source/src/env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="astro/client" />
+
+import { EnvType } from "astro.config";
+
+interface ImportMeta {
+  readonly env: EnvType;
+}
