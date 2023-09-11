6bc391c3cbda99e50c1c5b6387fbc572ca33c1aa
Michael Kassabov <35264484+Mkassabov@users.noreply.github.com>
2023-09-11 01:05:56 -0400

    zod => typebox

--- source/astro.config.ts
+++ source/astro.config.ts
@@ -2,15 +2,19 @@ import { getTargetEnv } from "@aerofoil/aerofoil-core/util/getTargetEnv";
 import cloudflare from "@astrojs/cloudflare";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
+import { Type as t } from "@sinclair/typebox";
+import { TypeCompiler } from "@sinclair/typebox/compiler";
 import { defineConfig } from "astro/config";
-import { z } from "zod";
 
 // todo add t3-env: https://github.com/t3-oss/t3-env/blob/main/examples/astro/src/t3-env.ts
-export const envValidation = z.object({});
+export const envValidationSchema = t.Object({});
+const envValidation = TypeCompiler.Compile(envValidationSchema);
 
-const env = await getTargetEnv(import.meta.url, envValidation);
+const env = await getTargetEnv<typeof envValidationSchema, typeof envValidation>(import.meta.url, envValidation);
 //@ts-ignore
 process.env = { ...process.env, ...env };
+
+export type EnvType = typeof env;
 
 // https://astro.build/config
 export default defineConfig({
--- source/src/env.d.ts
+++ source/src/env.d.ts
@@ -1,13 +1,12 @@
 /// <reference types="astro/client" />
 
-import type { envValidation } from "astro.config.ts";
+import type { EnvType } from "astro.config.ts";
 import type { ReadonlyDeep } from "type-fest";
-import type { z } from "zod";
 
 //* we declare global because imports make it a module
 // eslint-disable-next-line no-restricted-syntax
 declare global {
-	interface ImportMetaEnv extends ReadonlyDeep<z.infer<typeof envValidation>> {}
+	interface ImportMetaEnv extends ReadonlyDeep<EnvType> {}
 
 	// eslint-disable-next-line no-unused-vars
 	interface ImportMeta {
